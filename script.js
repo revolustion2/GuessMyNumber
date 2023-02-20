7a4437eafe811a27b0308774e5a5a90593cf0cf2
revolustion2 <narongkorn.knight0405@gmail.com>
2023-02-20 17:53:18 +0700

    update

--- script.js
+++ script.js
@@ -19,11 +19,19 @@ document.querySelector('.check').addEventListener
 ('click',function () {
   const guess =  Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
-
+  // When there is no input
   if (!guess) {
     document.querySelector('.message').textContent = '⛔️ No number';
+
+    // When player wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉 Correct Number!';
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+
+    document.querySelector('.number').style.width = '30rem';
+
+    // When guess is too high
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '📈 Too high!'; 
@@ -34,6 +42,7 @@ document.querySelector('.check').addEventListener
       document.querySelector('.score').textContent = 0;
     }
     
+    // When guess is too low
   } else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '📈 Too low!'; 
